51e6d15f4c789cba0bb73cc2e3e9e9d41a0c57c8
Andrew Layton <andrewlayton@Andrews-Hackintosh.local>
2019-06-22 14:55:01 -0400

    Intial commit

---
+++ phoneAndEmailScraper.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import re, pyperclip
+
+#TODO: Create a regex for phone numbers
+re.compile(r'''
+#000-000-0000, 000-0000, (000) 000-0000, 000-0000 ext 0000, ext. 0000, x0000
+
+#area code (optional)
+#first seperator
+''',re.VERBOSE)
+
+#TODO: Create a regex for email addresses
+
+#TODO: Get the text off the clipboard
+
+#TODO: Extract the email addresses and phone numbers from this text.
+
+#TODO: Copy the extracted email/phone to the clipboard
