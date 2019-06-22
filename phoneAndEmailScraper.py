e16000941d1253dae508e68df5e05b54402510d8
Andrew Layton <andrewlayton@Andrews-Hackintosh.local>
2019-06-22 15:06:43 -0400

    Defined regular expression for phone number types

--- phoneAndEmailScraper.py
+++ phoneAndEmailScraper.py
@@ -6,9 +6,17 @@ import re, pyperclip
 re.compile(r'''
 #000-000-0000, 000-0000, (000) 000-0000, 000-0000 ext 0000, ext. 0000, x0000
 
-#area code (optional)
-#first seperator
+((\d\d\d) | (\(\d\d\d\)))?        # area code (optional)
+(\s|-)        # first seperator
+\d\d\d        # first three digits
+-        # seperator
+\d\d\d\d        # last 4 digits
+(((ext(\.)?\s)|x)        # extension word-part(optional)
+(\d{2,5}))?      #extension number-part 
+
 ''',re.VERBOSE)
+
+
 
 #TODO: Create a regex for email addresses
 
